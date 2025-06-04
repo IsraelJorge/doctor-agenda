2d56df95773b6f975d0bb142f52109d1fb03b5f4
IsraelJorge <israeljorge2000@gmail.com>
2025-06-04 09:59:46 -0300

    refactor: clean up clinic form component by removing unused code and improving structure

--- src/app/(authenticated)/clinic-form/components/clinic-form.tsx
+++ src/app/(authenticated)/clinic-form/components/clinic-form.tsx
@@ -7,14 +7,6 @@ import { toast } from 'sonner'
 
 import { createClinic } from '@/actions/create-clinic'
 import { Button } from '@/components/ui/button'
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from '@/components/ui/card'
 import {
   Dialog,
   DialogContent,
@@ -59,85 +51,46 @@ export function ClinicForm() {
   }
 
   return (
-    <>
-      <Dialog open>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Adicionar clínica</DialogTitle>
-            <DialogDescription>
-              Adicione uma clínica para continuar.
-            </DialogDescription>
-          </DialogHeader>
-          <Form {...form}>
-            <form
-              id="clinic-form"
-              onSubmit={form.handleSubmit(handleCreateClinic)}
-            >
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
+    <Dialog open>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Adicionar clínica</DialogTitle>
+          <DialogDescription>
+            Adicione uma clínica para continuar.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            id="clinic-form"
+            onSubmit={form.handleSubmit(handleCreateClinic)}
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
 
-          <DialogFooter>
-            <Button
-              type="submit"
-              form="clinic-form"
-              disabled={form.formState.isSubmitting}
-              isLoading={form.formState.isSubmitting}
-            >
-              Criar clínica
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-      {/* <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleCreateClinic)}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Adicionar clínica</CardTitle>
-              <CardDescription>
-                Adicione uma clínica para continuar.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </CardContent>
-            <CardFooter>
-              <Button
-                className="w-full"
-                type="submit"
-                disabled={form.formState.isSubmitting}
-                isLoading={form.formState.isSubmitting}
-              >
-                Criar clínica
-              </Button>
-            </CardFooter>
-          </Card>
-        </form>
-      </Form> */}
-    </>
+        <DialogFooter>
+          <Button
+            type="submit"
+            form="clinic-form"
+            disabled={form.formState.isSubmitting}
+            isLoading={form.formState.isSubmitting}
+          >
+            Criar clínica
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
